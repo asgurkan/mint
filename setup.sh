5afc0297adfd772121bcf5daaf80f08583626e82
asgurkan <a.gurkan@massivebioinformatics.com>
2025-04-12 21:30:51 +0300

    correction

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit   # Exit immediately if a command exits with a non-zero status
-set -o nounset   # Treat unset variables as an error when substituting
-set -o pipefail  # Pipeline returns the exit status of the last command that fails
-
-conda env create -f mint_setup.yaml  
-conda activate mint 
